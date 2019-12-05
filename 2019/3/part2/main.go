5354e36974aa663b72100a31ef917fd311613bb3
Miguel Perez <miguel@miguelpz.com>
2019-12-04 22:56:11 -0500

    Add day 3 solutions

---
+++ 2019/3/part2/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	RIGHT = "R"
+	LEFT  = "L"
+	UP    = "U"
+	DOWN  = "D"
+)
+
+type point struct {
+	x, y, steps int
+}
+
+type wire struct {
+	points    []point
+	lastPoint point
+}
+
+func (w *wire) Step(direction string) error {
+	p := point{}
+	switch direction {
+	case RIGHT:
+		p.x = w.lastPoint.x + 1
+		p.y = w.lastPoint.y
+	case LEFT:
+		p.x = w.lastPoint.x - 1
+		p.y = w.lastPoint.y
+	case UP:
+		p.x = w.lastPoint.x
+		p.y = w.lastPoint.y + 1
+	case DOWN:
+		p.x = w.lastPoint.x
+		p.y = w.lastPoint.y - 1
+	default:
+		return fmt.Errorf("unknow direction %s", direction)
+	}
+
+	p.steps = w.lastPoint.steps + 1
+	w.points = append(w.points, p)
+	w.lastPoint = p
+	return nil
+}
+
+type Panel struct {
+	intersectionPoints []point
+}
+
+func (p *Panel) process(input io.Reader) error {
+	scanner := bufio.NewScanner(input)
+	firstWire := &wire{}
+	firstWireProcessed := false
+	lastPoint := point{}
+	for scanner.Scan() {
+
+		values := strings.TrimSpace(scanner.Text())
+		actions := strings.Split(values, ",")
+
+		for _, action := range actions {
+			w := &wire{lastPoint: lastPoint}
+			direction := string(action[0])
+			move, err := strconv.Atoi(action[1:])
+
+			if err != nil {
+				return fmt.Errorf("unable to process action %s", action)
+			}
+
+			for i := 0; i < move; i++ {
+				if firstWireProcessed {
+					w.Step(direction)
+					lastPoint = w.lastPoint
+					continue
+				}
+				firstWire.Step(direction)
+			}
+			if firstWireProcessed {
+				p.intersections(firstWire, w)
+			}
+		}
+		firstWireProcessed = true
+	}
+
+	return nil
+}
+
+func (p *Panel) shortersDistance() int {
+	distance := 0
+
+	abs := func(x int) int {
+		if x < 0 {
+			return -x
+		}
+		return x
+	}
+
+	for _, point := range p.intersectionPoints {
+		d := abs(point.x) + abs(point.y)
+		if distance == 0 || d < distance {
+			distance = d
+		}
+	}
+	return distance
+}
+
+func (p *Panel) shortersSteps() int {
+	steps := 0
+
+	for _, point := range p.intersectionPoints {
+		if steps == 0 || point.steps < steps {
+			steps = point.steps
+		}
+	}
+	return steps
+}
+
+func (p *Panel) intersections(wireOnePoints, wireTwoPoints *wire) {
+	for _, wireOnePoint := range wireOnePoints.points {
+		if wireOnePoint.x == 0 && wireOnePoint.y == 0 {
+			continue
+		}
+		for _, wireTwoPoint := range wireTwoPoints.points {
+			if wireTwoPoint.x == 0 && wireTwoPoint.y == 0 {
+				continue
+			}
+			if wireOnePoint.x == wireTwoPoint.x && wireOnePoint.y == wireTwoPoint.y {
+				p.intersectionPoints = append(p.intersectionPoints, point{wireTwoPoint.x, wireOnePoint.y, wireOnePoint.steps + wireTwoPoint.steps})
+				break
+			}
+		}
+	}
+}
+
+func New(input io.Reader) (*Panel, error) {
+	p := &Panel{}
+	err := p.process(input)
+	return p, err
+}
+
+func main() {
+	inputPathPtr := flag.String("input-path", "input", "path to input file")
+	stepsPtr := flag.Bool("steps", false, "Return the shorters steps")
+
+	flag.Parse()
+
+	input, err := os.Open(*inputPathPtr)
+
+	if err != nil {
+		fmt.Printf("Failed to open input file %s with %v\n", *inputPathPtr, err)
+		return
+	}
+	defer input.Close()
+
+	p, err := New(input)
+
+	if err != nil {
+		fmt.Printf("Failed to create panel with %s\n", err)
+	}
+
+	if !*stepsPtr {
+		fmt.Println(p.shortersDistance())
+		return
+	}
+
+	fmt.Println(p.shortersSteps())
+}
