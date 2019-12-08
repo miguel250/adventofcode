66094326fabccccc5da5c762840a27835eee4632
Miguel Perez <miguel@miguelpz.com>
2019-12-08 16:22:13 -0500

    Remove unnecessary function
    - remove unused field

--- 2019/8/part1/main.go
+++ 2019/8/part1/main.go
@@ -21,7 +21,7 @@ type image struct {
 
 func (img *image) parse(input io.Reader) error {
 	scanner := bufio.NewScanner(input)
-	scanner.Split(splitOnDigit)
+	scanner.Split(bufio.ScanRunes)
 	img.layers = make([]pixels, 0)
 	p := make(pixels, img.tall)
 
@@ -86,17 +86,6 @@ func (img *image) checkCorruption() int {
 		}
 	}
 	return oneCounter * twoCounter
-}
-
-func splitOnDigit(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	for i := 0; i < len(data); i++ {
-		return i + 1, []byte{data[i]}, nil
-	}
-	if !atEOF {
-		return 0, nil, nil
-	}
-	return 0, data, bufio.ErrFinalToken
-
 }
 
 func main() {
--- 2019/8/part2/main.go
+++ 2019/8/part2/main.go
@@ -18,7 +18,6 @@ type image struct {
 	wide         int
 	tall         int
 	minZeroLayer pixels
-	topLayers    []pixels
 }
 
 func (img *image) String() string {
@@ -42,7 +41,7 @@ func (img *image) String() string {
 
 func (img *image) parse(input io.Reader) error {
 	scanner := bufio.NewScanner(input)
-	scanner.Split(splitOnDigit)
+	scanner.Split(bufio.ScanRunes)
 	img.layers = make([]pixels, 0)
 	p := make(pixels, img.tall)
 
@@ -125,17 +124,6 @@ func (img *image) decode() string {
 
 	img.layers = []pixels{p}
 	return img.String()
-}
-
-func splitOnDigit(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	for i := 0; i < len(data); i++ {
-		return i + 1, []byte{data[i]}, nil
-	}
-	if !atEOF {
-		return 0, nil, nil
-	}
-	return 0, data, bufio.ErrFinalToken
-
 }
 
 func main() {
