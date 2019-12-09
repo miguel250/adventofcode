7bb31746a0b9bdb653768319ff0a02c77e161fb8
Miguel Perez <miguel@miguelpz.com>
2019-12-09 07:27:19 -0500

    2019: add day 9 solutions

---
+++ 2019/9/main.go
@@ -0,0 +1,476 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const maxMemory = 2048
+
+const (
+	additionInstruction       = 1
+	multiplicationInstruction = 2
+	saveInstruction           = 3
+	outputInstruction         = 4
+	jumpIfTrueIntruction      = 5
+	jumpIfFalseIntruction     = 6
+	lessThanIntruction        = 7
+	equalsInstruction         = 8
+	adjustRelativeBase        = 9
+	haltInstruction           = 99
+)
+
+const (
+	positionMode = iota
+	immediateMode
+	relativeMode
+)
+
+type program struct {
+	opsCodes            []int
+	initialState        []int
+	position            int
+	firstParameter      int
+	secondParameter     int
+	memoryAddress       int
+	output              []int
+	modes               []int
+	currentModePosition int
+	relativeModeBase    int
+}
+
+func (p *program) parser(input io.Reader, increaseMemory bool) error {
+	scanner := bufio.NewScanner(input)
+	scanner.Split(splitOnComma)
+
+	if increaseMemory {
+		p.opsCodes = make([]int, maxMemory)
+	}
+	memoryPosition := 0
+
+	for scanner.Scan() {
+		opsCode, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+
+		if err != nil {
+			return fmt.Errorf("failed to covered opsCode to an integer with: %w", err)
+		}
+
+		if increaseMemory {
+			p.opsCodes[memoryPosition] = opsCode
+			memoryPosition++
+		}
+
+		if !increaseMemory {
+			p.opsCodes = append(p.opsCodes, opsCode)
+		}
+		p.initialState = make([]int, len(p.opsCodes))
+		copy(p.initialState, p.opsCodes)
+	}
+	return nil
+}
+
+func (p *program) eval(inputValue []int) error {
+	currentInputIndex := 0
+
+	for p.hasNext() {
+		opsCode := p.next()
+
+		p.modes = make([]int, 0, 2)
+		p.modes = append(p.modes, (opsCode/10000)%10)
+		p.modes = append(p.modes, (opsCode/1000)%10)
+		p.modes = append(p.modes, (opsCode/100)%10)
+		p.currentModePosition = 2
+		opsCode = (opsCode % 100)
+
+		switch opsCode {
+		case haltInstruction:
+			return nil
+		case additionInstruction:
+			err := p.processOpsCode()
+			if err != nil {
+				return err
+			}
+
+			result := p.firstParameter + p.secondParameter
+			err = p.store(result, p.memoryAddress)
+
+			if err != nil {
+				return err
+			}
+
+		case multiplicationInstruction:
+			err := p.processOpsCode()
+
+			if err != nil {
+				return err
+			}
+
+			result := p.firstParameter * p.secondParameter
+			err = p.store(result, p.memoryAddress)
+
+			if err != nil {
+				return err
+			}
+
+		case saveInstruction:
+			position := p.next()
+			err := p.store(inputValue[currentInputIndex], position)
+			if err != nil {
+				return err
+			}
+			currentInputIndex++
+		case outputInstruction:
+			position := p.next()
+
+			value, err := p.getValue(position)
+			if err != nil {
+				return err
+			}
+
+			p.output = append(p.output, value)
+		case jumpIfTrueIntruction, jumpIfFalseIntruction:
+			position := p.next()
+
+			value, err := p.getValue(position)
+			if err != nil {
+				return err
+			}
+
+			jump := false
+			if opsCode == jumpIfTrueIntruction && value != 0 {
+				jump = true
+			}
+
+			if opsCode == jumpIfFalseIntruction && value == 0 {
+				jump = true
+			}
+
+			position = p.next()
+			if jump {
+				value, err := p.getValue(position)
+				if err != nil {
+					return err
+				}
+				p.position = value
+			}
+		case lessThanIntruction, equalsInstruction:
+			position := p.next()
+
+			firstValue, err := p.getValue(position)
+			if err != nil {
+				return err
+			}
+
+			position = p.next()
+
+			secondValue, err := p.getValue(position)
+			if err != nil {
+				return err
+			}
+
+			result := 0
+
+			if opsCode == lessThanIntruction && firstValue < secondValue {
+				result = 1
+			}
+
+			if opsCode == equalsInstruction && firstValue == secondValue {
+				result = 1
+			}
+
+			memoryAddress := p.next()
+			err = p.store(result, memoryAddress)
+
+			if err != nil {
+				return nil
+			}
+		case adjustRelativeBase:
+			position := p.next()
+
+			value, err := p.getValue(position)
+			if err != nil {
+				return err
+			}
+			p.relativeModeBase = p.relativeModeBase + value
+		default:
+			return fmt.Errorf("invalid opscode %d position: %d", opsCode, p.position)
+		}
+	}
+	return nil
+}
+
+func (p *program) hasNext() bool {
+	return p.position < len(p.opsCodes)
+}
+
+func (p *program) processOpsCode() error {
+	firstPosition := p.next()
+	secondPosition := p.next()
+	p.memoryAddress = p.next()
+
+	firstParameter, err := p.getValue(firstPosition)
+	if err != nil {
+		return err
+	}
+
+	p.firstParameter = firstParameter
+
+	secondParameter, err := p.getValue(secondPosition)
+	if err != nil {
+		return err
+	}
+
+	p.secondParameter = secondParameter
+	return nil
+}
+
+func (p *program) next() int {
+	value := p.opsCodes[p.position]
+	p.position++
+	return value
+}
+
+func (p *program) nextMode() int {
+	if len(p.modes) == 0 || p.currentModePosition == -1 {
+		return positionMode
+	}
+	mode := p.modes[p.currentModePosition]
+	p.currentModePosition--
+	return mode
+}
+
+func (p *program) getValue(position int) (int, error) {
+
+	mode := p.nextMode()
+
+	if mode == immediateMode {
+		return position, nil
+	}
+
+	if mode == relativeMode {
+		position = p.relativeModeBase + position
+	}
+
+	if err := p.isValidPosition(position); err != nil {
+		return 0, err
+	}
+	return p.opsCodes[position], nil
+}
+
+func (p *program) store(value, position int) error {
+	mode := p.nextMode()
+
+	if mode == relativeMode {
+		position = p.relativeModeBase + position
+	}
+
+	if err := p.isValidPosition(position); err != nil {
+		return err
+	}
+	p.opsCodes[position] = value
+	return nil
+}
+
+func (p *program) isValidPosition(position int) error {
+	if position > len(p.opsCodes) || position < 0 {
+		return fmt.Errorf("failed to store value in position %d", position)
+	}
+	return nil
+}
+
+func (p *program) String() string {
+	var b bytes.Buffer
+	size := len(p.opsCodes)
+
+	for i, v := range p.opsCodes {
+		fmt.Fprintf(&b, "%d", v)
+		if size-1 > i {
+			fmt.Fprint(&b, ",")
+		}
+	}
+	return b.String()
+}
+func (p *program) reset() {
+	p.position = 0
+	p.memoryAddress = 0
+	p.firstParameter = 0
+	p.secondParameter = 0
+
+	p.output = make([]int, 0)
+	p.modes = make([]int, 0)
+	p.currentModePosition = 0
+
+	p.opsCodes = make([]int, len(p.initialState))
+	copy(p.opsCodes, p.initialState)
+}
+
+func splitOnComma(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	for i := 0; i < len(data); i++ {
+		if data[i] == ',' {
+			return i + 1, data[:i], nil
+		}
+	}
+	if !atEOF {
+		return 0, nil, nil
+	}
+	return 0, data, bufio.ErrFinalToken
+
+}
+
+func main() {
+	inputPathPtr := flag.String("input-path", "input", "path to input file")
+	inputValuePtr := flag.String("input-value", "1,0", "value to input to code")
+	sequencePtr := flag.Bool("sequence", false, "run phase settingsr sequence of inputs")
+	generatePtr := flag.Bool("generate", false, "generate sequence of inputs")
+	increaseMemoryPtr := flag.Bool("increase-memory", false, "increase memory usaged")
+	flag.Parse()
+
+	input, err := os.Open(*inputPathPtr)
+
+	if err != nil {
+		fmt.Printf("Failed to open input file %s with %v\n", *inputPathPtr, err)
+		return
+	}
+	defer input.Close()
+
+	inputsStr := strings.Split(*inputValuePtr, ",")
+	inputsInt := make([]int, 0)
+
+	for _, v := range inputsStr {
+		value, err := strconv.Atoi(v)
+
+		if err != nil {
+			fmt.Printf("failed to converted input to integer with %s\n", err)
+			return
+		}
+		inputsInt = append(inputsInt, value)
+	}
+
+	var (
+		result  string
+		output  []int
+		execErr error
+	)
+
+	if !*sequencePtr && !*generatePtr {
+		result, output, execErr = exec(input, inputsInt, *increaseMemoryPtr)
+	}
+
+	if *sequencePtr {
+		p := &program{}
+		err := p.parser(input, *increaseMemoryPtr)
+
+		if err != nil {
+			fmt.Printf("failed to parse opscode with %s\n", err)
+			return
+		}
+
+		result, output, execErr = execSequence(p, inputsInt)
+	}
+
+	if *generatePtr {
+		p := &program{}
+		err := p.parser(input, *increaseMemoryPtr)
+
+		if err != nil {
+			fmt.Printf("failed to parse opscode with %s\n", err)
+			return
+		}
+
+		result, output, execErr = generateMaxSequence(p, inputsInt)
+	}
+
+	if execErr != nil {
+		fmt.Printf("Failed to exec opscode with %v\n", execErr)
+		return
+	}
+
+	fmt.Println(result)
+	for _, v := range output {
+		fmt.Printf("Output: %d\n", v)
+	}
+}
+
+func exec(input io.Reader, inputValue []int, increaseMemory bool) (string, []int, error) {
+	p := program{}
+	err := p.parser(input, increaseMemory)
+
+	if err != nil {
+		return "", []int{}, err
+	}
+
+	err = p.eval(inputValue)
+
+	return p.String(), p.output, err
+}
+
+func generateMaxSequence(p *program, inputValue []int) (string, []int, error) {
+
+	var (
+		result    string
+		output    []int
+		maxOutput int
+	)
+
+	var n = len(inputValue) - 1
+	var i, j int
+	for c := 1; c < 120; c++ {
+
+		i = n - 1
+		j = n
+
+		for inputValue[i] > inputValue[i+1] {
+			i--
+		}
+		for inputValue[j] < inputValue[i] {
+			j--
+		}
+
+		inputValue[i], inputValue[j] = inputValue[j], inputValue[i]
+		j = n
+		i += 1
+
+		for i < j {
+			inputValue[i], inputValue[j] = inputValue[j], inputValue[i]
+			i++
+			j--
+		}
+
+		r, o, err := execSequence(p, inputValue)
+
+		if err != nil {
+			return "", []int{}, err
+		}
+
+		if o[0] > maxOutput {
+			maxOutput = o[0]
+			result = r
+			output = o
+		}
+	}
+
+	return result, output, nil
+}
+
+func execSequence(p *program, inputValue []int) (string, []int, error) {
+	var err error
+
+	currentInputValue := 0
+	for i := 0; i < len(inputValue); i++ {
+		p.reset()
+		err = p.eval([]int{inputValue[i], currentInputValue})
+
+		if err != nil {
+			return "", []int{}, err
+		}
+		currentInputValue = p.output[0]
+	}
+
+	return p.String(), p.output, err
+}
---
+++ 2019/9/main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestExec(t *testing.T) {
+	for _, test := range []struct {
+		input, want    string
+		inputValue     int
+		output         []int
+		increaseMemory bool
+	}{
+		{"1,0,0,0,99", "2,0,0,0,99", 0, []int{}, false},
+		{"2,3,0,3,99", "2,3,0,6,99", 0, []int{}, false},
+		{"2,4,4,5,99,0", "2,4,4,5,99,9801", 0, []int{}, false},
+		{"1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99", 0, []int{}, false},
+		{"1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50", 0, []int{}, false},
+		{"3,0,4,0,99", "4,0,4,0,99", 4, []int{4}, false},
+		{"1002,4,3,4,33", "1002,4,3,4,99", 0, []int{}, false},
+		{"1101,100,-1,4,0", "1101,100,-1,4,99", 0, []int{}, false},
+		{"3,9,8,9,10,9,4,9,99,-1,8", "3,9,8,9,10,9,4,9,99,1,8", 8, []int{1}, false},
+		{"3,9,8,9,10,9,4,9,99,-1,8", "3,9,8,9,10,9,4,9,99,0,8", 1, []int{0}, false},
+		{"3,9,7,9,10,9,4,9,99,-1,8", "3,9,7,9,10,9,4,9,99,1,8", 1, []int{1}, false},
+		{"3,9,7,9,10,9,4,9,99,-1,8", "3,9,7,9,10,9,4,9,99,0,8", 9, []int{0}, false},
+		{"3,3,1108,-1,8,3,4,3,99", "3,3,1108,1,8,3,4,3,99", 8, []int{1}, false},
+		{"3,3,1107,-1,8,3,4,3,99", "3,3,1107,1,8,3,4,3,99", 7, []int{1}, false},
+		{"109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", "", 0, []int{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99}, true},
+		{"1102,34915192,34915192,7,4,7,99,0", "", 0, []int{1219070632396864}, true},
+		{"104,1125899906842624,99", "", 0, []int{1125899906842624}, true},
+	} {
+		var b bytes.Buffer
+		b.WriteString(test.input)
+
+		inputValues := []int{test.inputValue}
+		if test.inputValue == 0 {
+			inputValues = []int{}
+		}
+		got, output, err := exec(&b, inputValues, test.increaseMemory)
+
+		if err != nil {
+			t.Fatalf("Failed input (%s) with err: %s", test.input, err)
+		}
+
+		if got != test.want && test.want != "" {
+			t.Errorf("Got %s but expected: %s", got, test.want)
+		}
+
+		for i, v := range test.output {
+			if v != output[i] {
+				t.Errorf("Got %d output but expected: %d", output[i], v)
+			}
+		}
+	}
+}
+
+func TestExecSequence(t *testing.T) {
+	for _, test := range []struct {
+		input, want        string
+		inputValue, output []int
+	}{
+		{
+			"3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0",
+			"3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,43210,43210",
+			[]int{4, 3, 2, 1, 0},
+			[]int{43210},
+		},
+		{
+			"3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+			"3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,54321,54320",
+			[]int{0, 1, 2, 3, 4},
+			[]int{54321},
+		},
+		{
+			"3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+			"3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,65210,65210,0",
+			[]int{1, 0, 4, 3, 2},
+			[]int{65210},
+		},
+	} {
+		var b bytes.Buffer
+		b.WriteString(test.input)
+
+		p := &program{}
+		err := p.parser(&b, false)
+		if err != nil {
+			t.Fatalf("failed to parse opscode with %s\n", err)
+		}
+
+		got, output, err := execSequence(p, test.inputValue)
+
+		if err != nil {
+			t.Fatalf("Failed input (%s) with err: %s", test.input, err)
+		}
+
+		if got != test.want {
+			t.Errorf("Got %s but expected: %s", got, test.want)
+		}
+
+		for i, v := range test.output {
+			if v != output[i] {
+				t.Errorf("Got %d output but expected: %d", output[i], v)
+			}
+		}
+	}
+}
